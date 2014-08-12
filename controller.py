a3ce535d8e1f621c40c4d4ac230e0aabba2c3756
Matt <bappy13@hotmail.com>
2014-08-12 13:37:49 -0400

    update controller.py, update game_client.py

--- controller.py
+++ controller.py
@@ -1,4 +1,41 @@
 
 class controller:
 
-    pass
+    def __init__(self, s):
+        pass
+
+    def mouse_click_dispatch():
+        pass
+
+    def update_view():
+        pass
+
+    def update_server():
+        pass
+
+    def update_model():
+        pass
+
+    def players_turn():
+        pass
+
+    def select():
+        pass
+
+    def move():
+        pass
+
+    def attack():
+        pass
+
+    def battle():
+        pass
+
+    def kill():
+        pass
+
+    def trigger():
+        pass
+
+    def recv_move():
+        pass
--- game_client.py
+++ game_client.py
@@ -1,5 +1,8 @@
 #has to display and read input, pass input along to server
-
+#will client have to multithread in order to talk to server and do graphics??
+import view
+import controller
+import thread
 import socket
 
 ip = 'localhost'
@@ -7,3 +10,14 @@ port = 8888
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((ip, port))
+
+#Make the view and controller
+view = view.View()
+
+#controller takes the socket as input so it can communicate w/ server
+controller = controller.Controller(s)
+
+
+
+
+s.close()
